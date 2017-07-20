77d2c2f474bf26ede4c81a3007c12e7f2975e457
Yerzhan Mazhkenov <yerzhan7@outlook.com>
2017-07-20 21:06:43 +0100

    Adding logging of ocalls
    Current error:
    Entering ocall_lstat
    Error - can't open database connection:
    unable to open database file

--- App/ocalls.c
+++ App/ocalls.c
@@ -1,19 +1,27 @@
 // This is a real implementation of ocalls
-
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 int ocall_lstat(const char *path, struct stat* buf){
+    printf("Entering %s\n", __func__);
     return lstat(path, buf);
+    printf("Exiting %s\n", __func__);
 }
 
 int ocall_stat(const char *path, struct stat* buf){
+    printf("Entering %s\n", __func__);
     return stat(path, buf);
+    printf("Exiting %s\n", __func__);
 }
 int ocall_fstat(int fd, struct stat* buf){
+    printf("Entering %s\n", __func__);
     return fstat(fd, buf);
+    printf("Exiting %s\n", __func__);
 }
 int ocall_ftruncate(int fd, off_t length){
+    printf("Entering %s\n", __func__);
     return ftruncate(fd, length);
+    printf("Exiting %s\n", __func__);
 }
--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -14,16 +14,13 @@ void ecall_open()
     int rc;
     // Opening database
     const char* dbname = "test.db";
-    ocall_println_string("Creating database connection inside enclave...");
+    ocall_println_string("Enclave: Creating database connection ...");
     rc = sqlite3_open(dbname, &db);
     if (rc) {
-      ocall_println_string("Error: Can't open database  - ");
+      ocall_println_string("Error - can't open database connection: ");
       ocall_println_string(sqlite3_errmsg(db));
       return;
     }
-
-
-
 
     const char* sql = "CREATE TABLE COMPANY(ID INT PRIMARY KEY NOT NULL,NAME TEXT NOT NULL, AGE INT NOT NULL, ADDRESS CHAR(50), SALARY REAL);";
     char *zErrMsg = 0;
